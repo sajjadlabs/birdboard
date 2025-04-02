d6ca09598fac8c539ecbf876382d63fff7a5664a
= <=>
2025-04-02 13:22:24 +0330

    feat: allow users to create projects

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ProjectController extends Controller
+{
+    public function index(): View
+    {
+        $projects = Project::all();
+
+        return view('projects.index', compact('projects'));
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        // validate (OnHold...)
+
+        $project = Project::create([
+            'title' => $request->title,
+            'description' => $request->description
+        ]);
+
+        return redirect('/projects');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Models\Project;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(ProjectController::class)->group(function () {
+    Route::get('/projects', 'index');
+    Route::post('/projects', 'store');
 });
