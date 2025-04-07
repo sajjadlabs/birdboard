1b4dc3313498b4eb17426bd9fb0bb42d1f2700e2
= <=>
2025-04-07 20:10:59 +0330

    Change navigation logo

--- resources/views/components/layouts/navigation.blade.php
+++ resources/views/components/layouts/navigation.blade.php
@@ -4,9 +4,9 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                <div class="flex items-center">
+                    <a href="{{ route('home') }}">
+                        <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="bird-board logo">
                     </a>
                 </div>
 
